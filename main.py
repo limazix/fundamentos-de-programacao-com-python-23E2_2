90f4fb2f1b982130af4ff5fc3b7c8321bc68accd
limazix <blimacardoso@gmail.com>
2023-04-26 11:06:19 +0000

    It creates an add numbers operation

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 # -*- coding: utf-8 -*-
 
 from first_code.hello_world import print_hello_world
+from operators.operators import add
 
 print_hello_world()
+
+print(add(1, 3.2))
---
+++ operators/operators.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+
+def add(a, b):
+    """ It add two numbers
+        
+        param a: First value
+        type a: int, float
+        param b: Second valor
+        type b: int, float
+        
+        return: int, float
+    """
+    if type(a) == str or type(b) == str:
+        raise TypeError()
+    return a + b
