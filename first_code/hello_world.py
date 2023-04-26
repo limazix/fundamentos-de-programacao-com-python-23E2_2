5b1808f9748ab4cf09cedbfb3c968ad775ad5773
limazix <blimacardoso@gmail.com>
2023-04-26 10:00:19 +0000

    It creates a function that prints Hello World!

---
+++ first_code/hello_world.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+
+
+def print_hello_world():
+    """It prints 'Hello World!' to the console"""
+    print("Hello World!")
---
+++ main.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+
+from first_code.hello_world import print_hello_world
+
+print_hello_world()
