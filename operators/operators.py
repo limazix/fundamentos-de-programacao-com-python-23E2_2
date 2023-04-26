81bd3c89da980458d107023810797fb1acb14614
limazix <blimacardoso@gmail.com>
2023-04-26 11:15:17 +0000

    It creates the subtraction operator

--- operators/operators.py
+++ operators/operators.py
@@ -14,3 +14,18 @@ def add(a, b):
     if type(a) == str or type(b) == str:
         raise TypeError()
     return a + b
+
+
+def substraction(a, b):
+    """ It substract two numbers
+        
+        param a: First value
+        type a: int, float
+        param b: Second valor
+        type b: int, float
+        
+        return: int, float
+    """
+    if type(a) == str or type(b) == str:
+        raise TypeError()
+    return a - b
