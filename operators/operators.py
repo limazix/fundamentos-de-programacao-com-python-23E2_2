c57df02648adb30eb3bee097652b63171b52e4e9
limazix <blimacardoso@gmail.com>
2023-05-03 09:45:48 +0000

    Moving operators content to its own folder

--- operators/operators.py
+++ operators/operators.py
@@ -1,46 +1,104 @@
 # -*- coding: utf-8 -*-
 
 
-def add(a: int, b: int) -> int:
-    """ It add two numbers
-        
-        param a: First value
-        type a: int
-        param b: Second valor
-        type b: int
-        
-        return: int
-    """
-    if type(a) == str or type(b) == str:
-        raise TypeError()
-    return a + b
+# 1. Operadores (https://www.w3schools.com/python/python_operators.asp)
 
+# 1.1. Tipos de dados
 
-def subtraction(a, b):
-    """ It subtract two numbers
-        
-        param a: First value
-        type a: int, float
-        param b: Second valor
-        type b: int, float
-        
-        return: int, float
-    """
-    if type(a) == str or type(b) == str:
-        raise TypeError()
-    return a - b
+# Conjunto dos números Inteiros [-inf +inf]
+1
+3
+-2
+print(3, 2, -5)
 
+print(type(2))
 
-def division(a, b):
-    """ It divides two numbers
-        
-        param a: First value
-        type a: int, float
-        param b: Second valor
-        type b: int, float
-        
-        return: int, float
-    """
-    if type(a) == str or type(b) == str:
-        raise TypeError()
-    return a / b
+# Conjunto dos números reais 1.1, 2.5...
+
+print('num real', type(2.5))
+
+print(type(2.0))
+
+# 1.2. Variáveis e Atribuições
+
+x = 3
+t = type(x)
+print(t, x)
+
+# 1.3. Operadores Aritiméticos
+
+# + soma
+# - subtração
+# / divisão
+# *
+
+a = 3
+b = 5
+
+x = a + b * b - a / b
+
+t = type(2.0 * 8)
+print(t, x)
+
+# ** Potencia
+# // floor
+
+x = 2**2
+
+print(x)
+
+x = a + b
+x += a
+
+# 1.4. Operadores Lógicos
+
+# or Ou
+# and E
+# not Oposto
+
+x = bool(1)
+y = bool(0)
+t = type(x and y)
+print(t)
+
+# 1.5. Operadores de Identidade
+
+# is -> é
+# is not -> não é
+
+print(type(x) is not type(y))
+
+# 1.6. Operadores de Comparação
+
+# ==
+# !=
+# >
+# <
+
+print(a, b, a > b)
+
+# 1.7. Operadores de Pertencimento
+
+# in -> pertence
+# not in -> não pertence
+
+# ver exemplo na sessão 2
+
+# 1.8. Operadores Binários
+
+# & e binária
+
+# | ou binario
+
+# >> shift right
+
+# << shift left
+
+# 000 -> 0
+# 001 -> 1
+# 010 -> 2
+# 010 -> 3
+# 100 -> 5
+# 110 -> 6
+x = a + b
+print(x, x << 2, x >> 2)
